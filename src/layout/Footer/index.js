3ac6e96aa573cc3ea9a40e576f18b6929d470fcd
polinamoore <p_gerasimenko@yahoo.com>
2021-09-04 19:37:04 +0100

    Add footer

---
+++ src/layout/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <div>
+            <h3>2021 <a href="https://github.com/team-vibe">Team Vibe</a></h3>
+        </div>
+    )
+}
+
+export default Footer;
