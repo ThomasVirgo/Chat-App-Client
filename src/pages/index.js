29a686b5b0696abaf74878dedb6f4ddc74049503
polinamoore <p_gerasimenko@yahoo.com>
2021-09-04 19:37:48 +0100

    Add pages index.js

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Account } from "./Account";
+export { default as LandingPage } from "./LandingPage";
+export { default as Login } from "./Login";
+export { default as Recommendations } from "./Recommendations";
+export { default as Register } from "./Register";
+export { default as SearchResults } from "./SearchResults";
