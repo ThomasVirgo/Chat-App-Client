91a4dda32334fe0ff1563f91ff33e7a39505267b
polinamoore <p_gerasimenko@yahoo.com>
2021-09-04 19:37:31 +0100

    Add navbar code

---
+++ src/layout/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <header>
+                <div>
+                    <h1>Vibe</h1>
+                </div>
+             </header>
+        </div>
+     );
+}
+ 
+export default Navbar;
