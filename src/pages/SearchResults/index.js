2676a8b85e1e766948d99b042ed8d6e7a37149e0
polinamoore <p_gerasimenko@yahoo.com>
2021-09-06 11:54:13 +0100

    start searchResults page

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { NavigationBar } from "../../layout"
 
 function SearchResults() {
     return (
+        <>
+        <NavigationBar />
         <h1>Search Results Page</h1>
+        </>
     );
 }
 
