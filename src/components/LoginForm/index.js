4495cdc327d13b74ea01d73df9fb2fe381acd7b8
Rafika <RTuremis@hotmail.co.uk>
2021-09-06 18:15:45 +0100

    add loginUser action to start implementing redux

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import { loginUser } from '../../actions';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -17,31 +18,31 @@ const LoginForm = () => {
 
   const handleSubmitClick = (e) => {
     e.preventDefault();
-    sendDetailsToServer()
+    sendDetailsToServer();
   };
 
-  async function sendDetailsToServer(){
-      const payload = {username, password}
-      const { data } = await axios.post('http://localhost:8000/users/login/', payload)
-      console.log(data.token)
-      if (data.token){
-        localStorage.setItem('token', data.token)
-        setShouldRedirect(true)
-      }
+  async function sendDetailsToServer() {
+    const payload = { username, password };
+    const { data } = await axios.post('http://localhost:8000/users/login/', payload);
+    console.log(data.token);
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+      setShouldRedirect(true);
+    }
   }
 
   return (
     <>
-    {shouldRedirect && <Redirect to='/account'/>}
-    <div>
-      <form>
-        <label htmlFor="username-input">Please enter your username.</label>
-        <input type="textfield" id="username-input" value={username} onChange={handleUsernameChange}></input>
-        <label htmlFor="password-input">Please enter your password.</label>
-        <input type="password" id="password-input" value={password} onChange={handlePasswordChange}></input>
-        <input type="submit" value="submit" onClick={handleSubmitClick} />
-      </form>
-    </div>
+      {shouldRedirect && <Redirect to="/account" />}
+      <div>
+        <form>
+          <label htmlFor="username-input">Please enter your username.</label>
+          <input type="textfield" id="username-input" value={username} onChange={handleUsernameChange}></input>
+          <label htmlFor="password-input">Please enter your password.</label>
+          <input type="password" id="password-input" value={password} onChange={handlePasswordChange}></input>
+          <input type="submit" value="submit" onClick={handleSubmitClick} />
+        </form>
+      </div>
     </>
   );
 };
