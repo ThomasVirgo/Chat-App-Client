efa67d8bbef0fe16846e0a947957bb3093e1e5ff
polinamoore <p_gerasimenko@yahoo.com>
2021-09-06 10:22:01 +0100

    Add api info and axios code to actions to test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,36 @@
 import axios from "axios";
 
-export const loadRestaurants = () => {
+googleAPIKey = process.env.REACT_APP_GOOGLE_API_KEY // use for geocoding and restaurants
+
+skiddleAPIKey = process.env.REACT_APP_SKIDDLE_API_KEY // use for events (festivals, gigs, musicals, comedy shows etc)
+
+
+restaurantsURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}&key=${googleAPIKey}`
+
+
+// load restaurants list by user's input location:
+export const loadRestaurants = async (location) => {
+    try {
+        const response = await axios.get(restaurantsURL);
+
+        const restaurants = response.data;
+
+        console.log(`GET: Here's the list of restaurants in ${location}`, restaurants);
+        
+        return restaurants;
+    } catch (errors) {
+        console.log(errors);
+    }
+}
+
+// need latitude and longitude for events listings:
+export const loadGeolocation = () => {
+    return
+}
+
+
+
+
+export const loadEvents = () => {
     return
 }
