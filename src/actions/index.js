56a2a346dfa21681456c365cdede2fc0ef70453a
polinamoore <p_gerasimenko@yahoo.com>
2021-09-06 14:12:47 +0100

    add api calls for geolocation and gigs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
-// googleAPIKey = process.env.REACT_APP_GOOGLE_API_KEY // use for geocoding and restaurants
-
-// skiddleAPIKey = process.env.REACT_APP_SKIDDLE_API_KEY // use for events (festivals, gigs, musicals, comedy shows etc)
-
-
-// restaurantsURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
-
 
 // load restaurants list by user's input location:
 export const loadRestaurants = async (location) => {
     try {
-        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}, restaurants&key=${process.env.REACT_APP_GOOGLE_API_KEY}`);
+        // const response = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants,${location},London&key=${process.env.REACT_APP_GOOGLE_API_KEY}`); 
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants,${location},London&key=AIzaSyBPKGIi1k-hDdWitqvRxpYmtSK7Eto2ue4`); 
 
         const restaurants = response.data;
 
@@ -24,13 +18,37 @@ export const loadRestaurants = async (location) => {
 }
 
 // need latitude and longitude for events listings:
-export const loadGeolocation = () => {
-    return
+export const loadGeolocation = async (location) => {
+    try {
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location},London&key=${process.env.REACT_APP_GOOGLE_API_KEY}`);
+
+        const latitude = results.geometry.location.lat
+        const longitude = results.geometry.location.lng
+        const geolocationString = `latitude=${latitude}&longitude=${longitude}`
+
+        console.log(`GET: Here's lattitude and longitude of ${location}: ${geolocationString}`);
+
+        return geolocationString;
+    } catch (errors) {
+        console.log(errors);
+    }
 }
 
 
 
 
-export const loadEvents = () => {
-    return
+export const loadGigs = async (location) => {
+    const locString = loadGeolocation(location)
+    console.log(locString);
+    try {
+        const response = await axios.get(`https://www.skiddle.com/api/v1/events/search/?&api_key=${process.env.REACT_APP_SKIDDLE_API_KEY}&${locString}&radius=5&eventcode=LIVE&order=distance`); 
+
+        const results = response.data;
+
+        console.log(`GET: Here's a list of gigs in ${location}: ${results}`);
+
+        return results;
+    } catch (errors) {
+        console.log(errors);
+    }
 }
