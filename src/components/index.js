fbb7d2018d51447c0ff8cb89eaa802668994efb2
Tom Virgo <tomcvirgo@gmail.com>
2021-09-06 10:12:21 +0100

    add new routes to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as CarouselContainer } from "./carousel";
 export { default as RegistrationForm } from "./registrationForm";
+export { default as LoggedOutRoute } from "./LoggedOutRoute";
+export { default as PrivateRoute } from "./PrivateRoute";
