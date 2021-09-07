05232888b27e92e4fcc89358e136d4ba97420878
Rafika <RTuremis@hotmail.co.uk>
2021-09-07 13:02:55 +0100

    Combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,10 @@
-export { default as authReducer } from "./authReducer";
+import { combineReducers } from 'redux';
+// import { authReducer } from './authReducer';
+// import { searchReducer } from './searchReducer';
+import { default as authReducer } from './authReducer';
+import { default as searchReducer } from './searchReducer';
+
+export default combineReducers({
+  authReducer,
+  searchReducer,
+});
