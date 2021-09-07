410f37c12985f1de8c346de9f0919d71280bffbb
Rafika <RTuremis@hotmail.co.uk>
2021-09-07 16:21:48 +0100

    Refactor reducer

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,6 +1,4 @@
 const initState = {
-  searchLocation: '',
-  searchCategory: '',
   searchResultsArray: [],
 };
 
@@ -9,7 +7,7 @@ const searchReducer = (state = initState, action) => {
     // case 'SEARCH':
     //   return { ...state, searchLocation: action.payload.location, searchCategory: action.payload.category, error: false };
     case 'GETRESULTS':
-      return { ...state, searchResultsArray: action.payload, error: false };
+      return { searchResultsArray: action.payload, error: false };
     case 'SET_ERROR':
       return { ...state, error: action.payload };
     default:
