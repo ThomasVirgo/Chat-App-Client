c6db857bae9339cd30af65b39ae6c8b868586910
polinamoore <p_gerasimenko@yahoo.com>
2021-09-08 14:03:55 +0100

    clean up recommendations page and add comments

--- src/pages/Recommendations/index.js
+++ src/pages/Recommendations/index.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { RestaurantRecommendationsCard } from '../../components'
 import { EventRecommendationsCard } from '../../components'
-// import { useSelector } from 'react-redux';
 
 
 const Recommendations = () => {
     const [restaurantResults, setRestaurantResults] = useState([]);
     const [eventResults, setEventResults] = useState([]);
-    // const [combinedResults, setCombinedResults] = useState([]);
 
     useEffect(() => {
         const fetchRestaurantRecommendations = async () => {
@@ -35,20 +33,17 @@ const Recommendations = () => {
         fetchEventRecommendations();
     }, [])
 
+    // randomizing the order of array elements:
     const combinedResults = [...restaurantResults, ...eventResults].sort((a, b) => 0.5 - Math.random());;
-    console.log(restaurantResults)
-    console.log(eventResults)
-    console.log(combinedResults);
 
-    // const combinedArray = 
-    
-    // const resultsArray = useSelector((state) => state.searchReducer.searchResultsArray);
-    // const resultsCards = resultsArray.map((result, idx) => result.category === 'restaurant' ? <div key={idx}><RestaurantRecommendationsCard result={result}/></div> : <div key={idx}><EventRecommendationsCard result={result}/></div>)
+    // console.log(restaurantResults)
+    // console.log(eventResults)
+    // console.log(combinedResults);
+
 
     const resultsCards = combinedResults.map((result, idx) => result.category === 'restaurant' ? <div key={idx}><RestaurantRecommendationsCard result={result}/></div> : <div key={idx}><EventRecommendationsCard result={result}/></div>)
       return (
         <>
-          {/* <h1>Recommendations Page</h1> */}
           {resultsCards}
         </>
       );
