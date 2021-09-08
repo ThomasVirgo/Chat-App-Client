599ee6fc7dc4771cc15d5e4a2008d005e05d5b99
polinamoore <p_gerasimenko@yahoo.com>
2021-09-08 13:56:25 +0100

    add string scrubber function

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,7 +31,7 @@ export const fetchSearchResults = (location, cat) => {
         const { data } = await axios.get(`http://localhost:8000/apis/restaurant-search/${location}/`);
         // if (data) => raise error if not
         let resultsArray = data;
-        console.log(data);
+        // console.log(data);
         dispatch({
           type: 'GETRESULTS',
           payload: resultsArray,
@@ -39,7 +39,7 @@ export const fetchSearchResults = (location, cat) => {
       } else if (cat === 'ALL') {
         const { data } = await axios.get(`http://localhost:8000/apis/event-search/${location}%20%london`);
         let resultsArray = data;
-        console.log(data);
+        // console.log(data);
         dispatch({
           type: 'GETRESULTS',
           payload: resultsArray,
@@ -47,7 +47,7 @@ export const fetchSearchResults = (location, cat) => {
       } else {
         const { data } = await axios.get(`http://localhost:8000/apis/event-search/${location}%20%london/${cat}`);
         let resultsArray = data;
-        console.log(data);
+        // console.log(data);
         dispatch({
           type: 'GETRESULTS',
           payload: resultsArray,
@@ -60,6 +60,22 @@ export const fetchSearchResults = (location, cat) => {
       });
     }
   };
+};
+
+
+// Helper scrubber function
+export const scrubStr = (str) => {
+  const cleanStr = str
+    .replaceAll("&quot;", '"')
+    .replaceAll("&#039;", "'")
+    .replaceAll("&#39;", "'")
+    .replaceAll("&eacute;", "e")
+    .replaceAll("&amp;", " & ")
+    .replaceAll("&ldquo;", '"')
+    .replaceAll("&rsquo;", "'")
+    .replaceAll("&rdquo;", '"')
+    .replaceAll("&hellip;", " ... ");
+  return cleanStr;
 };
 
 // Create a fetchfunc that takes in the two values in global state (loc and cat)
