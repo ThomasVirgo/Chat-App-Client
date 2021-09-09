c5758e04262213a57d1e55fabf868c702b68573d
polinamoore <p_gerasimenko@yahoo.com>
2021-09-09 23:14:56 +0100

    test Header

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -7,7 +7,7 @@ import './style.css'
 const Header = () => {
     return (
         <header>
-            <NavLink className='img__link' exact to='/'><img id='home-icon' aria-label="logo" src={goodvibes} alt='vibes-logo'/></NavLink>
+            <NavLink className='img__link' aria-label="home-link" exact to='/'><img id='home-icon' aria-label="logo" src={goodvibes} alt='vibes-logo'/></NavLink>
             <span id='nav-slogan'>Find London's best dating ideas</span>
             <Nav />
         </header>
