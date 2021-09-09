c37e2a7b92bbf903c103bc49be0e7c1914d46d2d
Rafika <RTuremis@hotmail.co.uk>
2021-09-09 14:16:16 +0100

    Landing page styling

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Nav} from '../';
 import { NavLink } from 'react-router-dom';
+import goodvibes from '../../img/vibee.gif';
 import './style.css'
 
 const Header = () => {
     return (
         <header>
-            <NavLink exact to='/'><h1 className='home-button'>Vibe</h1></NavLink>
+            <NavLink exact to='/'><img id='home-icon' src={goodvibes} alt='vibes-logo'/></NavLink>
+            {/* <h4 id='nav-slogan'>London's good Vibes dating spots</h4> */}
             <Nav />
         </header>
     )
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Slider, SearchForm } from "../../components";
-import goodvibes from '../../img/goodvibes.gif'
+import goodvibes from '../../img/vibee.gif'
 import axios from 'axios'
 import './style.css'
 
@@ -20,9 +20,9 @@ function LandingPage() {
     return (
         <>
             <SearchForm />
-            <div>
-                <img src={goodvibes} alt='vibes-logo'/>
-                <h3>You catching our vibe? When two good vibes people come together but can't find a place with the same good vibes, well, what good does that do anyone? So, we decided to have your back. Find the coolest gigs and coziest restaurants here, save them to your account and tick them off once you've visited them. Don't forget to let us know the answer to our only question: were they truly good vibes?</h3>
+            <div id='landing-content'>
+                <img id='brand-logo' src={goodvibes} alt='vibes-logo'/>
+                <h3>You catching our vibe? When two good people vibe together but can't find a place with the same good vibes, well, what good does that do anyone? So, we decided to have your back. Find the coolest gigs and coziest restaurants here, save them to your account and tick them off once you've visited them. Don't forget to let us know the answer to our only question: what were the vibes there like?</h3>
                 {/* <Slider data={sliderResults} /> */}
             </div>
         </>
