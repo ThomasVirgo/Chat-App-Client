30a31c51ce40bd09a19725a7bac15dce79b45cd2
Tom Virgo <tomcvirgo@gmail.com>
2021-09-09 16:08:43 +0100

    final style changes

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -7,8 +7,8 @@ import './style.css'
 const Header = () => {
     return (
         <header>
-            <NavLink exact to='/'><img id='home-icon' src={goodvibes} alt='vibes-logo'/></NavLink>
-            {/* <h4 id='nav-slogan'>London's good Vibes dating spots</h4> */}
+            <NavLink className='img__link' exact to='/'><img id='home-icon' src={goodvibes} alt='vibes-logo'/></NavLink>
+            <span id='nav-slogan'>Find London's best dating ideas</span>
             <Nav />
         </header>
     )
