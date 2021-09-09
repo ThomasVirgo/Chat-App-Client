7692be47b1ea4feaef9fe993b7153f8fc94092ee
polinamoore <p_gerasimenko@yahoo.com>
2021-09-09 23:14:35 +0100

    test Footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
     return (
         <div>
-            <h3>2021 <a href="https://github.com/team-vibe">Team Vibe</a></h3>
+            <h3>2021 <a href="https://github.com/team-vibe" aria-label="github-link">Team Vibe</a></h3>
         </div>
     )
 }
