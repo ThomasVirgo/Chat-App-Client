8e15413e30101915a8c49139f1fee145be1ff2e4
Tom Virgo <tom.virgo@Apollotyres.com>
2021-10-20 09:10:27 +0100

    change server url to deployed backend

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const SERVER_URL = 'http://localhost:8000';
+const SERVER_URL = 'https://chat-app-drf.herokuapp.com'; // http://localhost:8000
 
 async function requestRegister(data){
     try {
